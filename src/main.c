c1066a19bc668a6615581fdac88c017b991224d0
sdxsd <willmaguire@icloud.com>
2022-05-26 19:50:57 +0200

    Tested memory leaks. Stacks now function.

--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ static void	add_content(t_list *stack, char	*argv[])
 {
 	long	num;
 
-	num = ft_atoi("3");
+	num = ft_atoi(*argv);
 	ft_memmove(stack->content, &num, sizeof(long));
 	argv++;
 	if (stack->next)
@@ -57,7 +57,7 @@ static void	print_stack(void *content)
 	long	*long_content;
 
 	long_content = content;
-	ft_printf("+---+\n%d\n", long_content);
+	ft_printf("+---+\n%d\n", *long_content);
 }
 
 static t_list	*gen_stack(int stack_size)
@@ -71,7 +71,7 @@ static t_list	*gen_stack(int stack_size)
 	new_element = NULL;
 	while (iter < stack_size)
 	{
-		new_element = ft_lstnew(malloc(sizeof(long)));
+		new_element = ft_lstnew(calloc(sizeof(long), 1));
 		if (!new_element)
 			ft_lstclear(&first_element, &free);
 		ft_lstadd_back(&first_element, new_element);
@@ -93,6 +93,10 @@ int	main(int argc, char *argv[])
 	stack_1 = gen_stack(argc - 1);
 	stack_2 = gen_stack(argc - 1);
 	add_content(stack_1, argv + 1);
+	ft_printf("STACK 1:\n");
 	ft_lstiter(stack_1, &print_stack);
-	/* ft_lstiter(stack_2, &print_stack); */
+	ft_printf("+---+\nSTACK 2:\n");
+	ft_lstiter(stack_2, &print_stack);
+	ft_lstclear(&stack_1, &free);
+	ft_lstclear(&stack_2, &free);
 }
